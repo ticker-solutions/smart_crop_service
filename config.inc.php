301becd93647da6cf9176118b84802b324ddc75e
Garry Newball <gnewball@ticker.solutions>
2015-05-27 19:38:42 -0500

    Ticker Smart Crop Service first commit

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * FlashFoto PHP API SDK - Examples Config
+ * For FlashFoto APIv2
+ */
+/* Set this to your partner username */
+$cfg['partner_username'] = 'gnewball';
+/* Set this to your parner apikey */
+$cfg['partner_apikey'] = 'xYWNXk98ZWe3hNbQRleLvDNf4oER9kxx';
+/* Only change this if you are running your own instance of the API (include /api/) */
+$cfg['base_url'] = 'http://flashfotoapi.com/api/'; //Default: http://flashfotoapi.com/api/
+?>
